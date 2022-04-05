c6549e6315dc77b68e77482b4db7a172abe5048c
vikinando <97882741+vikinando@users.noreply.github.com>
2022-04-06 01:28:43 +0700

    Create KotlinIntroduction.kt

---
+++ minggu4/KotlinIntroduction.kt
@@ -0,0 +1,55 @@
+//Kotlin Hello World
+fun main(args : Array<String>) {
+    println("Hello, World!")
+}
+
+
+//Kotlin Data Types
+fun main(args : Array<String>) {
+
+    val flag = true
+    println("$flag")
+}
+
+
+//Kotlin Operators
+fun main(args: Array<String>) {
+    var number = 12
+
+    number *= 5   // number = number*5
+    println("number  = $number")
+}
+
+
+//Kotlin Type Conversion
+fun main(args : Array<String>) {
+    val number1: Int = 545344
+    val number2: Byte = number1.toByte()
+    println("number1 = $number1")
+    println("number2 = $number2")
+}
+
+
+//Kotlin Expression & Statements
+fun main(args: Array<String>) {
+
+    val a = 12
+    val b = 13
+    val max: Int
+
+    max = if (a > b) a else b
+    println("$max")
+}
+
+
+//Kotlin Comments
+fun main(args: Array<String>) {
+
+   println("Hello, World!")
+}
+
+
+//Kotlin Basic Input Output
+fun main(args : Array<String>) {
+    println("Kotlin is interesting.")
+}
